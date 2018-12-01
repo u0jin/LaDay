33385336a145f3a396b0385ac2c63f3f2cfdb0c6
pengin7384 <pengin7384@gmail.com>
2018-12-01 16:12:15 +0900

    Add Activities

---
+++ app/src/main/java/com/example/pengin/laday/LogoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pengin.laday
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LogoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logo)
+    }
+}
---
+++ app/src/main/java/com/example/pengin/laday/SHTActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pengin.laday
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SHTActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sht)
+    }
+}
